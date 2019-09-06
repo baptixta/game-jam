988765e7fe6b398a12e410da6032edf15b40d64f
Felipe Godoy <felipegodoyf@outlook.com>
2019-09-06 05:21:03 -0300

    Easter egg.

--- ProjetoUnity/Game Jam/Assets/Scripts/NPC.cs
+++ ProjetoUnity/Game Jam/Assets/Scripts/NPC.cs
@@ -6,11 +6,21 @@ public class NPC : MonoBehaviour
 {
     public float minDelay;
     public float maxDelay;
+    public GameObject DESTROCADOVersion;
 
     IEnumerator Start ()
     {
         yield return new WaitForSeconds (Random.Range(minDelay, maxDelay));
         GetComponent<SpriteRenderer>().flipX = !GetComponent<SpriteRenderer>().flipX;
         StartCoroutine (Start());
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Instantiate (DESTROCADOVersion, transform.position, transform.rotation);
+            Destroy (gameObject);
+        }
     }
 }

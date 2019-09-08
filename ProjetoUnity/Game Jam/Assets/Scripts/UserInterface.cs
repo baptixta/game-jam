33d32d1846bb71a8e73c4efff4e9f8d404ac57c8
Felipe Godoy <felipegodoyf@outlook.com>
2019-09-08 16:30:36 -0300

    elementos de cenário adicionados

---
+++ ProjetoUnity/Game Jam/Assets/Scripts/UserInterface.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserInterface : MonoBehaviour
+{
+    public Animator transitionAnimator;
+
+    public void CallTransition (string animationName)
+    {
+        transitionAnimator.SetTrigger (animationName);
+    }
+}

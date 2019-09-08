e1974351b2565a1d72c57c928d8911640c7e06e3
Felipe Godoy <felipegodoyf@outlook.com>
2019-09-08 18:45:32 -0300

    musica gohorse

---
+++ ProjetoUnity/Game Jam/Assets/Scripts/Song.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Song : MonoBehaviour
+{
+    public AudioSource intro;
+    public AudioSource loop;
+
+    public void Start ()
+    {
+        intro.Play();
+        StartCoroutine (PlayLoop());
+    }
+
+    IEnumerator PlayLoop ()
+    {
+        yield return new WaitForSeconds (intro.clip.length);
+        loop.Play();
+    }
+}
